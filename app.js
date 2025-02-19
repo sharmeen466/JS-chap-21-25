6b044476fad76e29c8a517374c9d5f301172bd84
sharmeen466 <sharmeennazeer2@gmail.com>
2025-02-19 23:42:05 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,49 @@
+//......................chap # 21-25..............................
+//.......................STRING METHODS............................
+
+
+//  Q   #   1
+
+// let firstName = prompt("your 1st name ")
+// let lastName = prompt("your last name ")
+
+// let fullName = firstName + " " + lastName
+
+// alert(fullName + " Welcome ")
+
+
+//  Q   #   2
+
+// let phone = prompt("what's your favorite mobile phone model?")
+
+
+// document.write("My favorite phone is: "+ phone + "<br />")
+// document.write("Length of string: "+ phone.length)
+
+
+//  Q   #   3
+
+// let string = "Pakistani"
+
+// document.write ("String: "+ string + "<br />")
+// document.write ("Index of 'n': "+ string.indexOf("n"))
+
+
+//  Q   #   4
+
+// let string = "Hello World"
+
+// document.write ("String: "+ string + "<br />")
+//  document.write ("Last Index of 'n': "+ string.lastIndexOf("l"))
+
+
+//  Q   #   5
+
+// let string = "Pakistani"
+
+// document.write ("String: "+ string + "<br />")
+// document.write ("Character at Index '3': "+ string.slice(3,4))
+
+
+//  Q   #   6
+
